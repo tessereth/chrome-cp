0127df87ad78b20262b57f36b3e08402f931cb74
Tessa Bradbury <tessa@buildkite.com>
2022-03-28 16:50:59 +1100

    Initial implementation

---
+++ README.md
@@ -0,0 +1,25 @@
+# Chrome Copy
+
+This Chrome extension adds a context menu item to copy a link to a page as smart HTML.
+This is useful for pasting into rich chat clients and documents.
+
+## Installation
+
+TODO
+
+## Usage
+
+### Selection mode
+
+Select the text you want as the link text, right click and choose "Copy selection as link". This will create
+an HTML link to the current page with the selected text and put it on the clipboard.
+
+### Automatic mode
+
+If no text is selected, you can right click and choose "Copy link to page". The extension will try and choose something
+useful as the link text.
+
+This currently works for:
+
+* Github pull requests
+* Linear issue pages
---
+++ menus.js
@@ -0,0 +1,67 @@
+const COPY_AS_HTML = "copy-as-html"
+const COPY_SELECTION_AS_HTML = "copy-selection-as-html"
+
+// WARNING: This is run in the page context. It doesn't have access to external variables.
+const copyAddressAsHTML = async (url, text) => {
+    const GITHUB_RE = new RegExp('^https://github.com/buildkite/(\\w+)/pull/(\\d+)')
+    const LINEAR_RE = new RegExp('^https://linear.app/buildkite/issue/(\\w+-\\d+)')
+
+    try {
+        if (!text) {
+            if (m = GITHUB_RE.exec(url)) {
+                text = `${m[1]}#${m[2]}`
+            } else if (m = LINEAR_RE.exec(url)) {
+                text = m[1]
+            } else {
+                text = url
+            }
+        }
+
+        const clipboardItem = new ClipboardItem({
+            "text/plain": new Blob([`${text} <${url}>`], { type: "text/plain" }),
+            "text/html": new Blob([`<a href="${url}">${text}</a>`], { type: "text/html" })
+        })
+        await navigator.clipboard.write([clipboardItem])
+        console.log("Copied to clipboard")
+    } catch(err) {
+        console.warn(err)
+    }
+}
+
+const onMenuClicked = async (info, tab) => {
+    console.log("item " + info.menuItemId + " was clicked")
+
+    const url = info.pageUrl
+
+    if (info.menuItemId === COPY_AS_HTML) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: copyAddressAsHTML,
+            args: [url]
+        })
+    } else if (info.menuItemId === COPY_SELECTION_AS_HTML) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: copyAddressAsHTML,
+            args: [url, info.selectionText]
+        })
+    }
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        "title": "Copy link to page",
+        "id": COPY_AS_HTML,
+        "contexts": ["page"]
+    })
+
+    chrome.contextMenus.create({
+        "title": "Copy selection as link",
+        "id": COPY_SELECTION_AS_HTML,
+        "contexts": ["selection"]
+    })
+
+    chrome.contextMenus.onClicked.addListener(
+        onMenuClicked
+    )
+})
